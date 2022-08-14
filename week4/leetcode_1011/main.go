004fc2eafebf5f52ea3f3c488325b56e9c0a7f15
JerryLee <lizhangyuh@gmail.com>
2022-08-14 23:14:24 +0800

    fix: week4

--- week4/leetcode_1011/main.go
+++ week4/leetcode_1011/main.go
@@ -51,3 +51,21 @@ func shipWithinDays(weights []int, days int) int {
 
 	return result
 }
+
+func findMin(nums []int) int {
+	left, right := 0, len(nums)-1
+
+	for left < right {
+		mid := (left + right) / 2
+		// 重复元素则去掉最右端的那个
+		if nums[mid] == nums[right] {
+			right--
+		} else if nums[mid] < nums[right] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return nums[right]
+}
